aec44d351751e8fe049501787dc9838277bec6bc
munitakm <renan.manshio@gmail.com>
2022-08-17 18:48:13 -0300

    saving in database one stock price and hashing ip adress

--- routes/api.js
+++ routes/api.js
@@ -1,20 +1,41 @@
 'use strict';
 const fetch = require('node-fetch');
+const bcrypt = require('bcrypt');
+const mongoose = require('mongoose');
 
+let stockPrice = "";
 module.exports = function (app) {
+
+const stockSchema = mongoose.Schema({stock: {type: String, required: true}, ip: {type: String, required: true}, likes: Number});
+const StockIp = mongoose.model('StockIp', stockSchema);
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-	let stockName = req.query.stock.toLowerCase();
-	let stockPrice = "";
-	console.log(stockName);
-	fetch(`https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/${stockName}/quote`).
-	then(data => data.json()).
-	then(response => {
-	stockPrice = response.latestPrice;
-	});
-	res.send({"stock": req.query.stock.toUpperCase(), "stockData": stockPrice, "likes": 99});
+	let ipAddress = "";
+	let stockList = req.query.stock;
+	let likeBox = req.query.like == "true"? true :  false;
+	console.log(req.query);
+	if(typeof stockList == "object") {
+		console.log("tem 2 preços");
+	
+	} else {
+		console.log("tem 1 preço");
+		bcrypt.hash(req.socket.remoteAddress, 12, (err, hash) => {
+		ipAddress = hash;
+		const request = new StockIp({stock: stockList.toUpperCase(), ip: hash});
+		request.save()
+		});
+		
 
+
+	}
+	let dbComparisson = "";
+	StockIp.findOne({stock: "GOOG"}, (err, res) => { 
+	console.log(res);
+	dbComparisson = res.ip
+	})
+	let result = bcrypt.compareSync(ipAddress, dbComparisson);
+	console.log("os ip são iguais?", result);
     });
     
 };
