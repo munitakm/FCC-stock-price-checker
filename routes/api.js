20ac409967d961371d2dae6b0c8580b1e8b5ddaf
munitakm <renan.manshio@gmail.com>
2022-08-18 16:21:10 -0300

    created logic function

--- routes/api.js
+++ routes/api.js
@@ -6,36 +6,58 @@ const mongoose = require('mongoose');
 let stockPrice = "";
 module.exports = function (app) {
 
-const stockSchema = mongoose.Schema({stock: {type: String, required: true}, ip: {type: String, required: true}, likes: Number});
-const StockIp = mongoose.model('StockIp', stockSchema);
+const likeSchema = mongoose.Schema({stockName: {type: String, required: true}, likes: {type: Number, default: 0}, ips: [String]});
+const Likes = mongoose.model('Likes', likeSchema);
 
   app.route('/api/stock-prices')
     .get(function (req, res){
-	let ipAddress = "";
-	let stockList = req.query.stock;
-	let likeBox = req.query.like == "true"? true :  false;
-	console.log(req.query);
-	if(typeof stockList == "object") {
-		console.log("tem 2 preços");
-	
-	} else {
-		console.log("tem 1 preço");
-		bcrypt.hash(req.socket.remoteAddress, 12, (err, hash) => {
-		ipAddress = hash;
-		const request = new StockIp({stock: stockList.toUpperCase(), ip: hash});
-		request.save()
-		});
-		
+		let ipAddress = bcrypt.hashSync(req.socket.remoteAddress, 12);
+		//console.log(typeof req.query.stock)
+		if(typeof req.query.stock != "object") {
+			if(req.query.likes == "true") {
 
+			}	
+		}
+		else {
 
-	}
-	let dbComparisson = "";
-	StockIp.findOne({stock: "GOOG"}, (err, res) => { 
-	console.log(res);
-	dbComparisson = res.ip
+		}
+
 	})
-	let result = bcrypt.compareSync(ipAddress, dbComparisson);
-	console.log("os ip são iguais?", result);
-    });
-    
-};
+	//Functions//
+	const sendInfo = function(st) {
+		Likes.findOne(st, (err, found) => {
+			if(err) console.log(err);
+			return {stock: found.stockName.toUpperCase(), likes: found.likes}
+		})
+	}
+	const addLike = function(st, ip) {
+		  findAndUpdate({stockName: st}, ({$push:{ips: ip}, $inc: {likes: 1}}));
+	}
+
+	const createStock = function(st, like, ip) {
+		like == "true"?
+			Likes.create({stockName: st, ips: [ip], likes: 1 })
+		:
+			Likes.create({stockName: st});
+	};
+
+	const validIp = function(st, ip, like) {
+		Likes.find({stockName: st}, (err, ipList) => {
+			if(ipList == null) return true;
+			for(n of ipList.ip) {
+				if(bcryp.compareSync(ip, n)) {
+					return  true;
+				};
+			} return false; 
+		})
+	};
+
+	const existStock = function(st) {
+		Likes.find({stockName: st}, (err, found) =>{
+			if(found == null) return false;
+			return true
+		})
+	};
+
+	//End Functions
+}
